a1ba1432f13c6d14b753f95c63dbf229d5e05d97
ADM_DELOBE Florent (0017819U_ADM) <0017819U_ADM@COMMUN.AD.SNCF.FR>
2023-02-21 18:42:21 +0100

    update for subdir

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,15 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // Replace "/assets" with "Gifterest-front/assets" in output files
+        chunkFileNames: 'Gifterest-front/assets/[name]-[hash].js',
+        assetFileNames: 'Gifterest-front/assets/[name]-[hash][extname]',
+        entryFileNames: 'Gifterest-front/assets/[name]-[hash].js',
+      }
+    }
   }
 })
